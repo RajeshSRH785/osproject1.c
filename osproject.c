0f87d7bb4a947b6c931fa0b2929bfe74e7da8660
RajeshSRH785 <62884396+RajeshSRH785@users.noreply.github.com>
2020-04-10 22:21:14 +0530

    Update osproject.c

--- osproject.c
+++ osproject.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 #include<unistd.h>
+#include<stdlib.h>
 #include<string.h>
 int main()
 {
-int c[10][7],temp[5];
-int i,j,pt[10],waiting_time[10],totalwaitingtime=0,rk[10],temp1,n;
+char c[10][7],temp[5];
+int i,j,processtime[10],waitingtime[10],totalwaitingtime=0,pr[10],temp1,n;
 float averagewaitingtime;
 int et[0];
 printf("enter no of processes:");
@@ -13,40 +14,41 @@ for(i=0;i<n;i++)
 {
 printf("enter process%d name:",i+1);
 scanf("%s",c[i]);
-printf("Enter process time:");
-scanf("%d",&pt[i]);
+printf("enter process time:");
+scanf("%d",&processtime[i]);
 printf("enter priority:");
-scanf("%d",&rk[i]);
+scanf("%d",&pr[i]);
 }
 for(i=0;i<n-1;i++)
 {
 for(j=i+1;j<n;j++)
 {
-if(rk[i]>rk[j])
+if(pr[i]>pr[j])
 {
-temp1=rk[i];
-rk[i]=rk[j];
-rk[j]=temp1;
-temp1=pt[i];
-pt[i]=pt[j];
-pt[j]=temp1;
+temp1=pr[i];
+pr[i]=pr[j];
+pr[j]=temp1;
+temp1=processtime[i];
+processtime[i]=processtime[j];
+processtime[j]=temp1;
 strcpy(temp,c[i]);
 strcpy(c[i],c[j]);
 strcpy(c[j],temp);
 }
 }
 }
-waiting_time[0]=0;
+waitingtime[0]=0;
 for(i=1;i<n;i++)
 {
-waiting_time[i]=waiting_time[i-1]+et[i-1];
-totalwaitingtime=totalwaitingtime+waiting_time[i]-1;
+waitingtime[i]=waitingtime[i-1]+et[i-1];
+totalwaitingtime=totalwaitingtime+waitingtime[i]-1;
 }  
-averagewaitingtime=(float) totalwaitingtime/n+1/2;  
-printf("Process Name\t Process Time\t priority      Waiting time\n");
+averagewaitingtime=(float)totalwaitingtime/n+1/2;  
+printf("process_name\t process_time\t    priority\t waiting_time\n");
 for(i=0;i<n;i++)  
 {
- printf(" %s\t       %d                  %d\t \t\t%d\n" ,c[i],pt[i],rk[i],waiting_time[i]);;
+printf(" \t%s\t\t %d\t\t %d\t\t %d\n" ,c[i],processtime[i],pr[i],waitingtime[i]);
 }
-printf("Total Waiting Time=%d\n Avg. Waiting Time=%f", totalwaitingtime,averagewaitingtime);
+printf("total waiting time=%d\n avg waiting time=%f",totalwaitingtime,averagewaitingtime);
+return 0;
 }
